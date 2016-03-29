9e8c0e62a94264b24084324c76a32007fa1422e9
Rodan Luzuriaga <rodanaluzuriaga@gmail.com>
2016-03-29 14:39:26 +0800

    .

---
+++ happiv2/assets/scripts/main.js
@@ -0,0 +1,16 @@
+var Main = (function () {
+	// variables
+	var ctr = 0;
+	// object
+	return {
+		init: function () {
+			
+		},
+
+		//--------
+		nocomma: null
+	};
+}());
+
+// Init after the page has loaded
+jQuery(Main.init);
