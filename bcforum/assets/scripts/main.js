652a6303a44f951fd1a3cbc8150fb230bf8f5b40
Rodan Luzuriaga <rodanaluzuriaga@gmail.com>
2016-09-19 17:44:08 +0800

    bc forum

---
+++ bcforum/assets/scripts/main.js
@@ -0,0 +1,17 @@
+var Main = (function () {
+	// variables
+	var ctr = 0;
+
+	// object
+	return {
+		init: function () {
+			
+		},
+
+		//--------
+		nocomma: null
+	};
+}());
+
+// Init after the page has loaded
+jQuery(Main.init);
