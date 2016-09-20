a6e2e9a3b069035ce467a4bbca0a8c5057ca1824
Rodan Luzuriaga <rodanaluzuriaga@gmail.com>
2016-09-20 13:39:27 +0800

    .

--- bcforum/assets/scripts/main.js
+++ bcforum/assets/scripts/main.js
@@ -5,8 +5,33 @@ var Main = (function () {
 	// object
 	return {
 		init: function () {
-			
+
+			Main.scrollDirection()
+
 		},
+
+		scrollDirection: function() {
+
+      var lastScrollTop = 0;
+      jQuery(window).scroll(function(event) {
+        var st = jQuery(this).scrollTop();
+
+				if (st > 0) {
+					$(".backtotop").show();
+				} else {
+					$(".backtotop").hide();
+				}
+        if (st > lastScrollTop) {
+          // $(".backtotop").hide();
+          // console.log("down");
+        } else {
+          // $(".backtotop").show();
+          // console.log("up");
+        }
+        lastScrollTop = st;
+      });
+
+    },
 
 		//--------
 		nocomma: null
