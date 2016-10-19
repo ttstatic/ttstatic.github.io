588bbb24f75995a8ee61425e1cea9828bc72f157
Rodan Luzuriaga <rodanaluzuriaga@gmail.com>
2016-10-19 16:17:34 +0800

    .

---
+++ ge/assets/scripts/main.js
@@ -0,0 +1,19 @@
+var Main = (function () {
+	// variables
+	var ctr = 0;
+
+	// object
+	return {
+		init: function () {
+
+
+
+		},
+
+		//--------
+		nocomma: null
+	};
+}());
+
+// Init after the page has loaded
+jQuery(Main.init);
