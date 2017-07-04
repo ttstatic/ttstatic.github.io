50a9539c2d63d5b686b749e6830df1f2b906b17a
Rodan Luzuriaga <rodanaluzuriaga@gmail.com>
2017-07-04 22:43:51 +0800

    .

---
+++ corelogic/assets/scripts/main.js
@@ -0,0 +1,21 @@
+var Main = (function () {
+	// variables
+	var ctr = 0;
+
+	// object
+	return {
+		init: function () {
+
+			$(window).scroll(function() {
+				
+			});
+
+		},
+
+		//--------
+		nocomma: null
+	};
+}());
+
+// Init after the page has loaded
+jQuery(Main.init);
